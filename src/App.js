00d18a50ad6110e14e8007716b5ccc3c0829d2f5
Crihstian Molina <crihstianmol@gmail.com>
2022-04-08 19:12:16 -0500

    Adding components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import ToDoCounter from "./components/ToDoCounter";
+import ToDoCreateButton from "./components/ToDoCreateButton";
+import ToDoItem from "./components/ToDoItem";
+import ToDoList from "./components/ToDoList";
+import ToDoSearch from "./components/ToDoSearch";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ToDoCounter/>
+      <ToDoSearch/>
+      <ToDoList>
+        <ToDoItem/>
+      </ToDoList>
+      <ToDoCreateButton/>
+    </>
   );
 }
 
---
+++ src/components/ToDoCounter.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ToDoCounter() {
+  return (
+    <div>ToDoCounter</div>
+  )
+}
